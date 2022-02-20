bb86b7cbb9f4112c4527b7dfb9b470fb806ab612
DavidSG <david_serna_83@hotmail.com>
2022-02-20 18:16:43 +0100

    creamos primer controlador y entidades y apis

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\RideRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiController extends AbstractController
+{
+    /**
+     * @Route("/api/rides", name="rides", methods={"GET"})
+     */
+    public function rides(RideRepository $rideRepository): Response
+    {
+        $rides = $rideRepository->findAll();
+        $response = [];
+        foreach ($rides as $ride) {
+            $response[] = [
+                'id' => $ride->getId(),
+                'name' => $ride->getName(),
+                'ccaa' => $ride->getCcaa(),
+                'location' => $ride->getLocation()
+            ];
+        }
+
+        return new JsonResponse(
+            $response
+        );
+    }
+
+    /**
+     * @Route("/api/ride/{id}", name="ride", methods={"GET"}, requirements={"id": "\d+"} )
+     */
+    public function ride(int $id, RideRepository $rideRepository): Response
+    {
+        $ride = $rideRepository->find($id);
+        $response = [
+            'id' => $ride->getId(),
+            'name' => $ride->getName(),
+            'ccaa' => $ride->getCcaa(),
+            'address' => $ride->getAddress(),
+            'telephone' => $ride->getTelephone(),
+            'email' => $ride->getEmail(),
+            'duration' => $ride->getDuration(),
+            'description' => $ride->getDescription(),
+            'level' => $ride->getLevel()
+        ];
+
+
+        return new JsonResponse(
+            $response
+        );
+    }
+}
---
+++ src/Entity/Ride.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RideRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RideRepository::class)
+ */
+class Ride
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $ccaa;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $location;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $telephone;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $duration;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $level;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCcaa(): ?string
+    {
+        return $this->ccaa;
+    }
+
+    public function setCcaa(string $ccaa): self
+    {
+        $this->ccaa = $ccaa;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(string $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(int $duration): self
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getLevel(): ?string
+    {
+        return $this->level;
+    }
+
+    public function setLevel(string $level): self
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+}
